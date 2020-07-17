11829ed93f7595f403119c120ccefdf4975f6d51
Ali Goldani <ali.goldani@ymail.com>
2020-07-18 00:23:27 +0430

    bug fix

--- logger/dnschecker.py
+++ logger/dnschecker.py
@@ -4,7 +4,7 @@ UDP_IP = "0.0.0.0"
 UDP_PORT = 53
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(UDP_IP, UDP_PORT)
+sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
