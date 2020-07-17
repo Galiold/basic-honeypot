b24b1e93a2723d0e3a0c40c9f27f2ec42942edec
Ali Goldani <ali.goldani@ymail.com>
2020-07-18 02:20:54 +0430

    update intrusion test

--- utils/intrusion_test.py
+++ utils/intrusion_test.py
@@ -1,12 +1,15 @@
 from requests import request
 import threading
 
-def send_req():
+def send_req(i):
+    print('Request {} started'.format(i))
     request('POST', 'http://galiold.ir:8000')
+    print('Request {} done'.format(i))
 
 for i in range(11):
-    t = threading.Thread(target=send_req)
+    t = threading.Thread(target=send_req, args=(i,))
     t.setDaemon(True)
     t.start()
-    if i == 10:
-        t.join()
+
+while True:
+    pass
